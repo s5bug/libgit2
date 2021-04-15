ed019b4058460a582f64b5714f185b83d3d61d74
Edward Thomson <ethomson@edwardthomson.com>
2021-04-15 01:23:07 +0100

    worktree: validate worktree paths
    Worktree paths need to fix within MAX_PATH always, regardless of
    `core.longpaths` setting.

--- src/worktree.c
+++ src/worktree.c
@@ -135,6 +135,9 @@ static int open_worktree_dir(git_worktree **out, const char *parent, const char 
 		goto out;
 	}
 
+	if ((error = git_path_validate_ondisk(NULL, dir)) < 0)
+		goto out;
+
 	if ((wt = git__calloc(1, sizeof(*wt))) == NULL) {
 		error = -1;
 		goto out;
